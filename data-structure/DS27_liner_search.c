992e79fe00de83851d9b0fdf9aa7ce667d20c157
tanuj <96124171+tanujkumai@users.noreply.github.com>
2022-10-01 17:50:39 +0530

    new

--- data-structure/DS27_liner_search.c
+++ data-structure/DS27_liner_search.c
@@ -12,30 +12,7 @@ int linearSearch(int arr[], int size, int element)
     return -1;
 }
 
-int binarySearch(int arr[], int size, int element)
-{
-    int low, mid, high;
-    low = 0;
-    high = size - 1;
-    // Keep searching until low <= high
-    while (low <= high)
-    {
-        mid = (low + high) / 2;
-        if (arr[mid] == element)
-        {
-            return mid;
-        }
-        else if (arr[mid] < element)
-        {
-            low = mid + 1;
-        }
-        else
-        {
-            high = mid - 1;
-        }
-    }
-    return -1;
-}
+
 
 int main()
 {
@@ -45,11 +22,6 @@ int main()
     int element = 56 ;
     int searchIndex = linearSearch(arr, size, element);
     printf("The element %d was found at index %d \n", element, searchIndex);
-    // Sorted array for binary search
-    // int arr[] = {1, 3, 5, 56, 64, 73, 123, 225, 444};
-    // int size = sizeof(arr) / sizeof(int);
-    // int element = 444;
-    // int searchIndex = binarySearch(arr, size, element);
-    // printf("The element %d was found at index %d \n", element, searchIndex);
+   
     return 0;
 }
